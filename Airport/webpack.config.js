3a022b2a22a8a1d8afb46f31e78403b9eafdc926
ptol86 <ah021714@gmail.com>
2020-08-17 15:06:30 +0300

    test

--- Airport/webpack.config.js
+++ Airport/webpack.config.js
@@ -9,7 +9,7 @@ module.exports = (env, argv) => {
     const config = {
         entry: "./src/index.jsx",
         output: {
-            path: __dirname + '/build',
+            path: `${__dirname}/build`,
             filename: "bundle.js",
             publicPath: '/',
         },
